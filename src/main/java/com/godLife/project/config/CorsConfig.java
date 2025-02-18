72499c03e939e2d03165c232c95221b59b7bd023
funimani <gmlaksgmlaks@naver.com>
2025-02-18 16:06:09 +0900

    2025/02/18 (hm) Merge: merge login and join function.

--- src/main/java/com/godLife/project/config/CorsConfig.java
+++ src/main/java/com/godLife/project/config/CorsConfig.java
@@ -1,8 +1,8 @@
 package com.godLife.project.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +17,11 @@ public class CorsConfig {
                         .allowedOrigins("http://localhost:3000") // React 주소 허용
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true); // 쿠키 포함 시 필요
+                        /*
+                        .allowedHeaders("*") // 모든 헤더 허용
+                        .exposedHeaders("*") // 클라이언트가 응답 헤더를 읽을 수 있도록 허용
+                        .maxAge(3600); // 1시간 동안 CORS 설정 캐싱
+                         */
             }
         };
     }
--- src/main/java/com/godLife/project/mapper/UserMapper.java
+++ src/main/java/com/godLife/project/mapper/UserMapper.java
@@ -3,10 +3,15 @@ package com.godLife.project.mapper;
 import com.godLife.project.dto.datas.UserDTO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
+    // 로그인
+    @Select("SELECT USER_IDX, USER_ID, USER_PW FROM USER_TABLE WHERE USER_ID = #{userId}")
+    UserDTO UserLogin(@Param("userId") String userId);
+
     // 회원가입
     @Insert("INSERT INTO USER_TABLE(USER_IDX, USER_NAME, USER_ID, USER_PW, USER_NICK, USER_EMAIL, JOB_IDX, TARGET_IDX, USER_PHONE, USER_GENDER)" +
             "VALUES (USER_SEQ.NEXTVAL, #{userName}, #{userId}, #{userPw}, #{userNick}, #{userEmail}, #{jobIdx}, #{targetIdx}, #{userPhone}, #{userGender})")

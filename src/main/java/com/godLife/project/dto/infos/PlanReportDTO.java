603e8d2d10a341127d43b517eda0a24db26fc218
funimani <gmlaksgmlaks@naver.com>
2025-03-18 18:17:08 +0900

    2025/03/18 (hm) Temp: 임시저장

--- src/main/java/com/godLife/project/dto/infos/PlanReportDTO.java
+++ src/main/java/com/godLife/project/dto/infos/PlanReportDTO.java
@@ -13,16 +13,16 @@ import java.time.LocalDateTime;
 @Data
 public class PlanReportDTO {
     @Schema(description = "신고자 인덱스", example = "1")
-    private int REPORTER_IDX;
+    private int reporterIdx;
 
     @Schema(description = "루틴 인덱스", example = "1")
-    private int PLAN_IDX;
+    private int planIdx;
 
     @Schema(description = "신고 사유", example = "신고 사유를 작성합니다.")
-    private String REPORT_REASON;
+    private String reportReason;
 
     @Schema(description = "신고 상태", example = "0 : 처리 중, 1 : 완료")
-    private int STATUS;
+    private int status;
 
     @Schema(description = "추천 시간 로그", example = "2025-02-16 HH:mm:ss")
     @JsonSerialize(using = LocalDateTimeSerializer.class) // 직렬화
